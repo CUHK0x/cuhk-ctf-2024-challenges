f4bec68128daf30ed0ebc0611d0f4c0024165dee
chemistrying <toto4801110@gmail.com>
2024-11-16 00:36:46 +0800

    Fix run_chall.sh errors when running in Kali

--- run_chall.sh
+++ run_chall.sh
@@ -1,9 +1,9 @@
 if [ -d $1 ]; then
-    if [[ ! -d $1/prod || "$2" == "--build" ]]; then
+    if [ ! -d $1/prod ] || [ "$2" = "--build" ]; then
         ./make_chall.sh $1
     fi
     cd $1/prod
-    if [ "$2" == "--build" ]; then
+    if [ "$2" = "--build" ]; then
         docker compose up -d --build
     else
         docker compose up -d
